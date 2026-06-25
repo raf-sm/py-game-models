08e6fb73ecfc5d61a1ec6d4680cf6998a5abc573
raf-sm <rafasmendonca007@gmail.com>
2026-06-25 15:29:52 -0300

    Solution

--- main.py
+++ main.py
@@ -1,11 +1,46 @@
-import init_django_orm  # noqa: F401
-
-from db.models import Race, Skill, Player, Guild
+import json
+from django.db import transaction
+from db.models import Race, Skill, Guild, Player
 
 
 def main() -> None:
-    pass
+    with open("players.json", "r") as f:
+        players_data = json.load(f)
+    with transaction.atomic():
+        for nickname, player_data in players_data.items():
+            race, _ = Race.objects.get_or_create(
+                name=player_data["race"]["name"],
+                defaults={
+                    "description": player_data["race"].get(
+                        "description", ""
+                    )
+                }
+            )
 
+            guild_data = player_data.get("guild")
+            guild = None
+            if guild_data:
+                guild, _ = Guild.objects.get_or_create(
+                    name=guild_data["name"],
+                    defaults={"description": guild_data.get("description")}
+                )
 
-if __name__ == "__main__":
-    main()
+            for skill_data in player_data["race"].get("skills", []):
+                Skill.objects.get_or_create(
+                    name=skill_data["name"],
+                    race=race,
+                    defaults={"bonus": skill_data["bonus"]}
+                )
+
+            Player.objects.get_or_create(
+                nickname=nickname,
+                defaults={
+                    "email": player_data["email"],
+                    "bio": player_data["bio"],
+                    "race": race,
+                    "guild": guild
+                }
+            )
+
+    if __name__ == "__main__":
+        main()
